1ffbfe27a940c928d282e0e16c92f860f101e3c3
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2021-09-02 12:24:32 +0000

    add i18n para titulos e confirmacao de delete

--- app/helpers/admins_template_helper.rb
+++ app/helpers/admins_template_helper.rb
@@ -1,2 +1,11 @@
 module AdminsTemplateHelper
+
+   def translate_attribute(object, method)
+      if object && method
+         object.model.human_attribute_name(method) 
+      else
+         "Informe os parâmetro corretamente."
+      end
+   end
+   
 end
